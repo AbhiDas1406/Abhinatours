81e35942e22a6dcba92a127f4a4d6ce1fabf7209
Abhi Das <abhidaskgpian@gmail.com>
2025-08-21 19:33:42 +0530

    deployment fixes

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successful!'));
 
